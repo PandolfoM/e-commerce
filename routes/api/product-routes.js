094853925cd389a100bc41db38f2f8d78a8954ad
Matthew Pandolfo <matt@pandolfo.com>
2021-10-21 15:01:39 -0400

    fixed foreign key issue

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,10 +8,15 @@ router.get("/", (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   Product.findAll({
+    attributes: ["id", "product_name", "price", "stock"],
     include: [
       {
         model: Category,
-        attributes: ["id", "category_name"],
+        attributes: ["category_name"],
+      },
+      {
+        model: Tag,
+        attributes: ["tag_name"],
       },
     ],
   })
